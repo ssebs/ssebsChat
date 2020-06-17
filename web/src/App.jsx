ed6e86526332a679e93a94ab0bb5e0effd52464f
Sebastian Safari <ssebs@ymail.com>
2020-06-17 14:30:29 -0700

    Basic header

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -6,18 +6,21 @@ import Home from "./components/Home";
 import Profile from "./components/Profile";
 import MsgWindow from "./components/MsgWindow";
 import NewConversation from "./components/NewConversation";
+import { Container } from "react-bootstrap";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Header />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/profile" component={Profile} />
-                <Route path="/convo/new" component={NewConversation} />
-                <Route path="/convo/:id" component={MsgWindow} />
-                <Redirect to="/" />
-            </Switch>
+            <Container>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/convo/new" component={NewConversation} />
+                    <Route path="/convo/:id" component={MsgWindow} />
+                    <Redirect to="/" />
+                </Switch>
+            </Container>
         </BrowserRouter>
     );
 };
--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { withRouter } from "react-router";
 
-export const Header = () => {
-    return <div>I'm the header</div>;
+export const Header = props => {
+    return (
+        <>
+            <Navbar bg="dark" variant="dark" sticky="top" expand="md">
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>ssebsChat</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse
+                        id="basic-navbar-nav"
+                        className="ml-auto d-flex justify-content-end"
+                        style={{ height: "32", maxHeight: "32" }}
+                    >
+                        <LinkContainer to="/convo/new">
+                            <Nav.Link>Create a new Conversation</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/profile">
+                            <Nav.Link>😃</Nav.Link>
+                        </LinkContainer>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 };
 
-export default Header;
+export default withRouter(Header);
